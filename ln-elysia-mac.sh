0b343d6e5a798e441e38dbc2b44414be43e04f3a
LinCenYing <lincenying@qq.com>
2026-04-30 16:08:43 +0800

    chore: update

--- ln-elysia-mac.sh
+++ ln-elysia-mac.sh
@@ -4,8 +4,8 @@ rm -rf .cursor/rules/global-00-general.mdc
 rm -rf .cursor/rules/global-01-backend.mdc
 rm -rf .cursor/rules/global-02-commit.mdc
 
-ln -s /Users/lincenying/Library/CloudStorage/OneDrive-个人/Setting/cusor/Elysia/rules/global-00-general.mdc .cursor/rules/global-00-general.mdc
-ln -s /Users/lincenying/Library/CloudStorage/OneDrive-个人/Setting/cusor/Elysia/rules/global-01-backend.mdc .cursor/rules/global-01-backend.mdc
-ln -s /Users/lincenying/Library/CloudStorage/OneDrive-个人/Setting/cusor/Elysia/rules/global-02-commit.mdc .cursor/rules/global-02-commit.mdc
+ln -s /Users/lincenying/OneDrive/Setting/cusor/Elysia/global-00-general.mdc .cursor/rules/global-00-general.mdc
+ln -s /Users/lincenying/OneDrive/Setting/cusor/Elysia/global-01-backend.mdc .cursor/rules/global-01-backend.mdc
+ln -s /Users/lincenying/OneDrive/Setting/cusor/Elysia/global-02-commit.mdc .cursor/rules/global-02-commit.mdc
 
 grep -qxF '.cursor/rules/global*.mdc' .gitignore 2>/dev/null || echo '.cursor/rules/global*.mdc' >> .gitignore
